b9fbbf889bce93eeb2a654eeb683e41a84ba5333
t168608 <nsharma@tcfbank.com>
2018-02-02 14:05:45 -0600

    First Checkin

---
+++ src/main/java/com/tcfbank/deposit_availability/DepositAvailabilityApplication.java
@@ -0,0 +1,12 @@
+package com.tcfbank.deposit_availability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositAvailabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositAvailabilityApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tcfbank/deposit_availability/DepositAvailabilityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tcfbank.deposit_availability;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepositAvailabilityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
