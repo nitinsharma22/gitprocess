8d4f9e782049fb21fda7c3a0dd39888e96c47135
t168608 <nsharma@tcfbank.com>
2018-02-02 14:14:03 -0600

    Change Package

--- src/main/java/com/tcfbank/deposits/DepositAvailabilityApplication.java
+++ src/main/java/com/tcfbank/deposits/DepositAvailabilityApplication.java
@@ -1,4 +1,4 @@
-package com.tcfbank.deposit_availability;
+package com.tcfbank.deposits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
